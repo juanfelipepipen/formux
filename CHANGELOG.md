cfcf564a8fc3e4f7bba9dba22f0db14cc385fb54
Juan Felipe Lopez Estrada <juanfelipepipen@gmail.com>
2024-11-18 16:53:15 -0700

    feat: formux cubit support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+* ## 0.0.2
+
+* Formux cubit support
+
 * ## 0.0.1
 
 * Initial release.
---
+++ lib/cubit/cubit_form.dart
@@ -0,0 +1,24 @@
+import 'package:pipen/formux/abstract/formux.dart';
+import 'package:formux/abstract/formux_copy.dart';
+import 'package:bloc/bloc.dart';
+
+abstract class CubitForm<F extends Formux> extends Cubit<F> {
+  CubitForm(super.initialState);
+
+  /// Validate form and if is valid, call method and
+  /// emit new state from parent
+  void validate(Function(F form) onValid) {
+    F form = state;
+    bool valid = form.valid();
+
+    if (valid) {
+      onValid.call(form);
+    }
+
+    if (F case FormuxCopy<F> coppier) {
+      emit(coppier.copy());
+    } else {
+      throw Exception("FormuxCopy is not defined in ${F.runtimeType}");
+    }
+  }
+}
