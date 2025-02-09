7d129ccd8e91185f2788200cc6e5cebeacafdaa1
Juan Felipe Lopez Estrada <juanfelipepipen@gmail.com>
2025-02-08 20:48:19 -0700

    feat: cubit form support

--- lib/cubit/cubit_form.dart
+++ lib/cubit/cubit_form.dart
@@ -1,4 +1,6 @@
+import 'package:cloux/managers/exceptions/input_errors_exception.dart';
 import 'package:formux/abstract/formux_copy.dart';
+import 'package:formux/abstract/formux_input_ids.dart';
 import 'package:formux/formux.dart';
 import 'package:bloc/bloc.dart';
 
@@ -15,8 +17,22 @@ abstract class CubitForm<F extends Formux> extends Cubit<F> {
       onValid.call(form);
     }
 
+    emit(_copyForm());
+  }
+
+  /// Add error to form with id
+  void inputErrors(InputErrorsException inputErrors) {
+    final form = _copyForm();
+    if (form case FormuxInputIds inputIds) {
+      inputErrors.form(inputIds);
+    }
+    emit(form);
+  }
+
+  /// Get copy at current form
+  F _copyForm() {
     if (state case FormuxCopy<F> coppier) {
-      emit(coppier.copy());
+      return coppier.copy();
     } else {
       throw Exception("FormuxCopy is not defined in ${F.runtimeType}");
     }
